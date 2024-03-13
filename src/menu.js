ffba1bf6ca6a949712b6a2a1db56ae7d7c686fc4
Jniemel <juho_n@live.fi>
2024-03-13 13:21:36 +0200

    Improved menu tab logic & added more styling

--- src/menu.js
+++ src/menu.js
@@ -1,33 +1,87 @@
 import './styles.css';
 
+function addToMenu(header, array) {
+    // add header
+    let placement = document.querySelector('.menu');
+    const heading = document.createElement('h1');
+    heading.textContent = header;
+    placement.appendChild(heading);
+
+    // create list of items
+    for (let i = 0; i < array.length; i++) {  
+        // create item: name, price & description)
+        let item = document.createElement('div');
+        item.classList.add('menu-item');
+        let itemRow1 = document.createElement('div');
+        itemRow1.classList.add('name-and-price');
+        let itemRow2 = document.createElement('div');
+        itemRow2.classList.add('description');
+        let name = document.createElement('h3');
+        name.textContent = array[i]["name"];
+        let price = document.createElement('h4');
+        price.textContent = array[i]["price"] + "€";
+        let description = document.createElement('h5');
+        description.textContent = array[i]["description"];
+        // append item to menu
+        itemRow1.append(name, price);
+        itemRow2.appendChild(description);
+        item.append(itemRow1, itemRow2);        
+        placement.appendChild(item);
+    }
+}
+
 let foodMenu = [
     {
         "name": "Burger-meal",
         "price": "12",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
     },
     {
         "name": "Spaghetti",
         "price": "10",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing."
     },
     {
-        "name": "pancakes",
-        "price": "5",
+        "name": "Steak",
+        "price": "25",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do."
+    },
+    {
+        "name": "Pizza",
+        "price": "15",
+        "description": "Lorem ipsum dolor sit amet, consectetur."
     }
 ];
 
 let drinkMenu = [
     {
-        "name": "Cola",
+        "name": "Soda",
         "price": "2",
+        "description": "Lorem ipsum dolor"
     },
     {
         "name": "Beer",
         "price": "4",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing."
     },
     {
         "name": "Wine",
         "price": "6",
+        "description": "Lorem ipsum dolor sit amet."
     }
+];
+
+let dessertMenu = [
+    {
+        "name": "Cupcakes",
+        "price": "3",
+        "description": "Lorem ipsum dolor"
+    },
+    {
+        "name": "Pancakes",
+        "price": "5",
+        "description": "Lorem ipsum dolor sit amet."
+    },
 ];
 
 function menuTab() {
@@ -45,34 +99,11 @@ function menuTab() {
     menuContainer.classList.add('menu');
     content.appendChild(menuContainer);
 
-    // add food items to menu
-    const headingFood = document.createElement('h1');
-    headingFood.textContent = "Food";
-    menuContainer.appendChild(headingFood);
-    for (let i = 0; i < foodMenu.length; i++) {
-        const foodItem = document.createElement('div');
-        foodItem.classList.add('menu-item');
-        const foodName = document.createElement('h3');
-        foodName.textContent = foodMenu[i]["name"];
-        const foodPrice = document.createElement('h4');
-        foodPrice.textContent = foodMenu[i]["price"] + "€";
-        foodItem.append(foodName, foodPrice);
-        menuContainer.appendChild(foodItem);
-    }
-    // add drink items to menu
-    const headingDrinks = document.createElement('h1');
-    headingDrinks.textContent = "Drinks";
-    menuContainer.appendChild(headingDrinks);
-    for (let j = 0; j < drinkMenu.length; j++) {
-        const drinkItem = document.createElement('div');
-        drinkItem.classList.add('menu-item');
-        const drinkName = document.createElement('h3');
-        drinkName.textContent = drinkMenu[j]["name"];
-        const drinkPrice = document.createElement('h4');
-        drinkPrice.textContent = drinkMenu[j]["price"] + "€";
-        drinkItem.append(drinkName, drinkPrice);
-        menuContainer.appendChild(drinkItem);
-    }
+    // add items to menu
+    addToMenu("Food", foodMenu);
+    addToMenu("Desserts", dessertMenu);
+    addToMenu("Drinks", drinkMenu);
+
 }
 
 export default menuTab;
